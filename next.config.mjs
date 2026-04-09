86b09ceb786b3c86f0a7f1950a0a497df0aad56c
mdnahidooo <md.nahidofficial.cse@gmail.com>
2026-04-09 15:55:28 +0600

    Image Optimization in Next.js

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,19 @@
 const nextConfig = {
   /* config options here */
   reactCompiler: true,
+
+  // https://unsplash.com/photos/a-small-island-with-palm-trees-in-calm-water-VU7z8SQsBTo
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'unsplash.com',
+        port: '',
+        pathname: '/**',
+        search: '',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,9 +1,17 @@
-
+import Image from "next/image";
+import heroImg from '../../assets/image/hero_img.jpg'
 
 const AboutPage = () => {
     return (
         <div>
-           <h2>About Page</h2> 
+            <h2 className="m-5 text-2xl">About Page</h2> 
+            
+            <Image className="mx-auto" width="1200" height="300" src="/banner.jpg" alt="Banner img"></Image>
+
+            <Image className="mx-auto h-100 mt-5" width="1200" height="300" src={heroImg} alt="hero img"></Image>
+
+
+            <Image width="1200" height="1000" src="https://unsplash.com/photos/a-small-island-with-palm-trees-in-calm-water-VU7z8SQsBTo" alt="online image"></Image>
         </div>
     );
 };
